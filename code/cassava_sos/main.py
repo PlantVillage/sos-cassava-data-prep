eaae338d1a095a2d746c0f2deffb151f371abb3a
eai6 <edwardamoahidun@gmail.com>
2023-06-05 10:20:08 +0300

    fixed bug in odkcentral util

--- code/cassava_sos/main.py
+++ code/cassava_sos/main.py
@@ -2,13 +2,13 @@ import os
 
 def main():
     print("Running planting survey....")
-    os.system("python3 code/cassava_sos/planting_survey.py")
+    os.system("python code/cassava_sos/planting_survey.py")
     print("\n\n")
     print("Running germination survey...")
-    os.system("python3 code/cassava_sos/germination_survey.py")
+    os.system("python code/cassava_sos/germination_survey.py")
     print("\n\n")
     print("Running farm visit survey...")
-    os.system("python3 code/cassava_sos/farm_visit_survey.py")
+    os.system("python code/cassava_sos/farm_visit_survey.py")
 
 
 if __name__ == "__main__":
