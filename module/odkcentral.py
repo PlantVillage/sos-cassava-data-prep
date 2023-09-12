8fb7174888d5706e4fc61dbacca100afcbbb4bca
Derek Morr <derekmorr@psu.edu>
2023-09-12 15:51:08 -0400

    remove unused method; add submisison filtering in download method

--- module/odkcentral.py
+++ module/odkcentral.py
@@ -1,10 +1,9 @@
-import requests
 import json
+import requests
 import zipfile
-import pandas as pd
 
 # Needs a bearer token to interact with the API
-def getBearerToken(url, auth):
+def getBearerToken(url: str, auth: str):
     headers = {
       'Content-Type': 'application/json'
     }
@@ -32,48 +31,24 @@ headers = {
 }
 
 
-def downloadSubmissions1(form_url, headers):
-    download_url = form_url + f"submissions.csv.zip?attachments=false"
-    form_file = f'{form_url.split("/")[-2]}.zip'
-    r = requests.post(download_url, headers=headers, stream=True)
-    with open(form_file, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=512):
-            if chunk:
-                f.write(chunk)
-
-    # unzip downloaded folder
-    with zipfile.ZipFile(form_file,"r") as zip_ref:
-        zip_ref.extractall("")
-
-    return pd.read_csv(f'{form_url.split("/")[-2]}.csv')
-
-def downloadSubmissions(form_url, headers=headers):
-    download_url = form_url + f"submissions.csv.zip?attachments=false"
+def downloadSubmissions(form_url, headers=headers) -> str:
+    download_url = form_url + "submissions.csv.zip"
     form_file = f'odk_temp_download/{form_url.split("/")[-2]}.zip'
+    params = {
+        'attachments': False,
+        '$filter': "__system/reviewState ne 'rejected' and __system/reviewState ne 'hasIssues'"
+    }
     try:
-        r = requests.post(download_url, headers=headers, stream=True)
+        r = requests.get(download_url, headers=headers, stream=True, params=params)
         with open(form_file, 'wb') as f:
             for chunk in r.iter_content(chunk_size=512):
                 if chunk:
                     f.write(chunk)
 
-        # unzip downloaded folder
-        with zipfile.ZipFile(form_file,"r") as zip_ref:
+        with zipfile.ZipFile(form_file, "r") as zip_ref:
             zip_ref.extractall(f"odk_temp_download/{form_url.split('/')[-2]}")
     except:
-        print("Failed to downlaod new")
+        print("Failed to download new")
         pass
 
     return f'odk_temp_download/{form_url.split("/")[-2]}'
-
-
-def downloadFormSubmissions(form_url, auth=auth):
-    if bearerToken == "":
-        bearerToken = getBearerToken(sessions_url, auth)
-
-        headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {bearerToken}'
-        }
-
-    return downloadSubmissions(form_url, headers)
